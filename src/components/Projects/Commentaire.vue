f484d2582904ac183f11d0c6fd5eb80c9d95bfa4
Anthony Frere <anthony.frere@hotmail.com>
2024-07-16 11:41:02 +0200

    src/components/Projects/Commentaire.vue

--- src/components/Projects/Commentaire.vue
+++ src/components/Projects/Commentaire.vue
@@ -3,7 +3,7 @@
         <img src="@/assets/images/Espacecomm.png" alt="espace commentaire">
         <div class="projet_text">
             <h2>Lien :</h2>
-            <a href="https://github.com/ToynShad/Dynaespacecommentjs/blob/main/index.html">https://github.com/ToynShad/Dynaespacecommentjs/blob/main/index.html</a>
+            <a href="https://github.com/ToynShad/Dynaespacecommentjs/blob/main/index.html" target="_blank">https://github.com/ToynShad/Dynaespacecommentjs/blob/main/index.html</a>
             <h2>Description</h2>
             <p>L'objectif de ce projet était de créer la partie front-end de l'intégration d'un nouveau commentaire dans un espace commentaire. 
                 Cela implique la mise en place d'un formulaire de saisie, la gestion des erreurs, l'affichage des commentaires.</p>
--- src/components/Projects/Projets.vue
+++ src/components/Projects/Projets.vue
@@ -3,7 +3,7 @@
         <img src="@/assets/images/cv.jpg" alt="cv">
         <div class="projet_text">
             <h2>Lien :</h2>
-            <a href="https://github.com/ToynShad/cvegnligne2">https://github.com/ToynShad/cvegnligne2</a>
+            <a href="https://github.com/ToynShad/cvegnligne2" target="_blank">https://github.com/ToynShad/cvegnligne2</a>
             <h2>Description</h2>
             <p>Ce projet vise à convertir un CV numérique en format HTML et CSS en vue de son intégration dans une
                 application web professionnelle. L'objectif est de créer un CV en ligne attrayant et accessible sur
